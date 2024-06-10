70f9b9897c8b218d128395dcc18953dd0fc002f9
AkshayTukaral <aktukaral@gmail.com>
2024-06-10 09:15:53 +0530

    Tasks given in class 10/06/2024

---
+++ Task1.py
@@ -0,0 +1,8 @@
+# 1. Explain the difference between the = operator and the == operator in Python.
+#  = used as a assignment operator and == is used as comparison operator
+
+# What does the ** operator do in Python, and how is it used?
+# ** is used for assign the power to any specific number
+
+# What does the ^ operator do in Python, and in what context is it commonly used?
+# the "^" operator is the bitwise XOR operator. It performs a bitwise XOR operation between two integers.
